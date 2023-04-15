a8206aad5e4c4e5d70743ff703c19a2a8fd0c704
Sediqa <sadat.hadid2000@gmail.com>
2023-04-15 09:27:31 +0000

    Add  prevState

--- my-app/src/components/Stateful.js
+++ my-app/src/components/Stateful.js
@@ -1,34 +1,59 @@
 import React from "react";
 
 class Stateful extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      introduction: "Hello!",
+      buttonText: "Exit",
+      count: 0,
+    };
+  }
 
-    constructor(props){
-        super(props)
-        this.state = {
-            instorduction: "Heya!",
-            buttonText: "Exit",
-        }
-    }
-    handleClick(){
-        this.setState({
-            instorduction: "Goodebye!",
-            buttonText: "Enter",
-        }, ()=> {
-        console.log('New state: ',this.state.instorduction);
-        console.log('New state: ',this.state.buttonText);
+  handleClick() {
+    this.setState((prevState, prevProps) => {
+      console.log("Previous State:", prevState);
+      console.log("Previous Props:", prevProps);
+      return {
+        introduction:
+          prevState.introduction === "Hello!" ? "Goodbye!" : "Hello!",
+        buttonText: prevState.buttonText === "Exit" ? "Enter" : "Exit",
+      };
+    });
+  }
 
-        });
-        console.log(this.state.instorduction);
-        console.log(this.state.buttonText);
-    }
-    render(){
-        return(
-            <div>
-                <h1>{this.state.instorduction} {this.props.greetings}</h1>
-                <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
-            </div>
-        )
-    }
+  increment() {
+    this.setState((prevState, prevProps) => {
+      console.log("Previous State:", prevState);
+      console.log("Previous Props:", prevProps);
+      return {
+        count: prevState.count + 1,
+      };
+    });
+  }
+
+  incrementFive() {
+    this.increment();
+    this.increment();
+    this.increment();
+    this.increment();
+    this.increment();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>
+          {this.state.introduction} {this.props.greeting}
+        </h1>
+        <button onClick={() => this.handleClick()}>
+          {this.state.buttonText}
+        </button>
+        <button onClick={() => this.increment()}>Increment</button>
+        <p>You've clicked {this.state.count} times</p>
+      </div>
+    );
+  }
 }
 
 export default Stateful;
